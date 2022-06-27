6f1e63d9c6fb23969db15f350e3b031b38218466
nisargdongare <drnfriend@gmail.com>
2022-06-27 23:05:53 +0530

    logo updated

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -8,7 +8,7 @@ const ListScreen = () => {
         <View style={styleList.container}>
             <HeaderBox title='List of Transactions' />
             <View style={{ paddingTop: 5 }}>
-                <ShortCard name='Nisarg' amount={48} currency='$' date='2021-04-22' time='02:15' status='completed' logo="https://raw.githubusercontent.com/nisargdongare/ListDesign/main/src/logo/logo51.svg" />
+                <ShortCard name='Nisarg' amount={48} currency='$' date='2021-04-22' time='02:15' status='completed' logo="https://raw.githubusercontent.com/nisargdongare/ListDesign/main/src/logo/31.svg" />
                 <ShortCard name='Pavan Dongare' amount={740} currency='$' date='2021-04-22' time='04:50' status='completed' logo='https://raw.githubusercontent.com/nisargdongare/ListDesign/main/src/logo/logo4.svg' />
                 <ShortCard name='Amruta Avinash Chavan' amount={30} currency='$' date='2021-04-22' time='02:10' status='unsuccessful' logo='https://raw.githubusercontent.com/nisargdongare/ListDesign/main/src/logo/logo6.svg' />
             </View>
